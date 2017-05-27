aac9bcb897fec87f9fc1e723473bddc8682dd305
andi1up <andi1up@me.com>
2017-05-27 18:01:37 -0400

    Sketch.js
    Added a Boost percentage. 
    
    You can use the boost once it reaches 100.
    
    Press ( X ) or ( , ) to activate it depending on your respective side.
    Players will only have 1 chance when it comes to using it, after the next ball bounce, you will lose it.
    
    To gain percentage, you have to bounce it on your paddle 5 times for 5 percent, or score for 12 percent.

--- sketch.js
+++ sketch.js
@@ -1,14 +1,20 @@
 //Pong
 var isPaused = false;
 var paddleSpeed = 9;
-var bounceCount = 0;
+var bounceCount = 0;   
+		var boostMeter1 = 0;	// My intelligence level
+		var boostMeter2 = 0;	// isn't high enough
+		var boostCount1 = 0;	// to find a simplar solution.
+		var boostCount2 = 0;	// Gotta wait for my next level up and use my points much more wisely.
 var paddleLeft = {
 	length: 0,
 	x:80,
 	y:0,
 	thickness:30,
 	direction:0,
-	score:0
+	score:0,
+	level:0,
+	boostOn:false
 };
 
 var paddleRight = {
@@ -17,7 +23,9 @@ var paddleRight = {
 	y:0,
 	thickness:30,
 	direction:0,
-	score:0
+	score:0,
+	level:0,
+	boostOn:false
 };
 
 var ball = {
@@ -28,6 +36,7 @@ var ball = {
 	ySpeed: 8,
 	radius:11
 };
+
 
 
 
@@ -77,35 +86,59 @@ function draw() {
 
 	if((ball.x + ball.radius > paddleLeft.x && ball.x - ball.radius < paddleLeft.x + paddleLeft.thickness) && ball.y > paddleLeft.y  && ball.y < paddleLeft.y+ paddleLeft.length){
 		var degree = -1 * ((paddleLeft.y + paddleLeft.length/2) - ball.y) / (paddleLeft.length/2) * 60;//Calculates distance from the middle of the paddle and maps it on value 0-60.
-		if(degree > -10 && degree < 10){
-			ball.speed *= 1.5;
+		if(paddleLeft.boostOn == true){			//The sweet spot is now achieved by getting your Boost Percent higher
+			if(degree > -10 && degree < 10){	//The player will only have 1 try to get that shot. Totally not being lazy or anything.
+				ball.speed *= 1.5;
+			}
+			paddleLeft.boostOn = false;
 		}
 		ball.xSpeed = ball.speed * cos(degree);
 		ball.ySpeed = ball.speed * sin(degree);
+		boostCount1++;
 		bounceCount++;
 	}
 
 
 	if(ball.x + ball.radius > paddleRight.x && ball.x - ball.radius < paddleRight.x + paddleRight.thickness && ball.y > paddleRight.y  && ball.y < paddleRight.y+ paddleRight.length){
 		var degree = -1 * ((paddleRight.y + paddleRight.length/2) - ball.y) / (paddleRight.length/2) * 60;//Calculates distance from the middle of the paddle and maps it on value 0-60.
-		if(degree > -10 && degree < 10){
-			ball.speed *= 1.5;
+		if(paddleRight.boostOn == true){		//The sweet spot is now achieved by getting your Boost Percent higher
+			if(degree > -10 && degree < 10){
+				ball.speed *= 1.5;
+			}
+			paddleRight.boostOn = false;
 		}
 		ball.xSpeed = -1 * ball.speed * cos(degree);
 		ball.ySpeed = ball.speed * sin(degree);
 
-
+		boostCount2++;
 		bounceCount++;
 	}
 
 	if(bounceCount === 5){
-		ball.speed *= 1.19;		//Played around with the value, I think this is suitable
-		paddleSpeed *= 1.05;	//Having the paddle move a lil faster gets fun!
+		ball.speed *= 1.19;				//Played around with the value, I think this is suitable
+		paddleSpeed *= 1.05;			//Having the paddle move a lil faster gets fun!
 		bounceCount = 0;
 	}
+	
+	if(boostCount1 === 5){
+		
+		boostCount1 = 0;
+		if(paddleLeft.level < 100){		//Makes sure it doesn't exceeds over 100.
+			paddleLeft.level += 5;		//Every 5 hits will give 5 percent for the boost
+		}
+	}
+	if(boostCount2 === 5){				//Take a wild guess
+		boostCount2 = 0;
+		if(paddleRight.level < 100){
+			paddleRight.level += 5;
+		}
+	}
+	
+	
 
 	if(ball.x - ball.radius/2 > windowWidth){
-		paddleLeft.score +=1;
+		paddleLeft.score +=1;					
+		paddleLeft.level += 12;						//The meter will jump up 12 points
 		ball.x = windowWidth/2;
 		ball.y = windowHeight/2
 		ball.speed = 10;
@@ -116,11 +149,17 @@ function draw() {
 		var leftRight = random(0,2);
 		ball.xSpeed = cos(degree) * ball.speed * -1;
 		ball.ySpeed = sin(degree) * ball.speed;
+		
+		if(paddleLeft.level > 100){					//Meter will remain 100
+		paddleLeft.level = 100;
+		}
+		
 		isPaused = true;						//Paused the game for every score
 	}
 
 	if(ball.x + ball.radius/2 < 0){
-		paddleRight.score +=1;
+		paddleRight.score +=1;							
+		paddleRight.level += 12;				//The meter will jump up 12 points
 		ball.x = windowWidth/2;
 		ball.y = windowHeight/2;
 		ball.speed = 10;
@@ -130,6 +169,12 @@ function draw() {
 		var degree = random(-60,61);
 		ball.xSpeed = cos(degree) * ball.speed;
 		ball.ySpeed = sin(degree) * ball.speed;
+		
+		if(paddleRight.level > 100){					//Meter will remain at 100
+		
+		paddleRight.level = 100;
+		}
+		
 		isPaused = true;						//Paused the game for every score
 	}
 }
@@ -146,9 +191,14 @@ function draw() {
 	textSize(32);
 	textAlign(LEFT);
 	text("Player 1: " + paddleLeft.score , 0,50);
+	text("Boost%: " + paddleLeft.level, 0,110);
+	rect(10, 120, paddleLeft.level, 2);	//Boost Meter (LEFT)
 	textAlign(RIGHT)
 	text("Player 2: " + paddleRight.score, windowWidth,50)
-
+	text("Boost%: " + paddleRight.level, windowWidth,110);
+	rect(windowWidth - 220, 120, paddleRight.level, 2);	//Boost Meter (RIGHT)
+	
+	
 	
 
 
@@ -167,6 +217,19 @@ function keyPressed() {										//Noticed when holding W, S won't work until yo
   else if(keyIsDown(75)){
   	paddleRight.direction = paddleSpeed;
   }
+    if(keyCode == 88){				//In the future, this can activate tht sweet spot.
+	  if(paddleLeft.level >= 100){	//Just incase paddleLeft.level somehow goes above 100.
+			paddleLeft.level = 0;	
+			paddleLeft.boostOn = true;
+	  }
+	}
+	
+	if(keyCode == 188){
+		if(paddleRight.level >= 100){
+			paddleRight.level = 0;
+			paddleRight.boostOn = true;
+		}
+	}
 
   if(keyCode == 32){
   	if(isPaused){
@@ -176,6 +239,9 @@ function keyPressed() {										//Noticed when holding W, S won't work until yo
   	}
   }
   return false; // prevent default
+  
+
+  
 }
 
 function keyReleased(){
